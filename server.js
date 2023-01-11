8276bf1459ab3495d6460be4494a863e2445cfdf
Olawale <67635601+luscious-dev@users.noreply.github.com>
2023-01-12 00:39:18 +0100

    Created the home page

---
+++ server.js
@@ -0,0 +1,20 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config({ path: `${__dirname}/config.env` });
+
+// /api/v1/users
+
+// /api/v1/users/:id
+
+// /api/v1/blog/posts
+
+// /api/v1/blog/posts/:id
+
+// /api/v1/blog/posts/:id/comments
+
+// /api/v1/blog/posts/:postid/comments/:commentid
+
+const port = process.env.APP_PORT || 8000;
+app.listen(process.env.APP_PORT, () => {
+  console.log(`LISTENING ON PORT ${port}...`);
+});
